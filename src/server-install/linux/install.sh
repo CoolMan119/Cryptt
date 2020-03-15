0cbdbc51b41cf24ec13bcef64196b2a76159332f
houseofkraft <CoolMan119@users.noreply.github.com>
2020-03-15 14:44:00 -0400

    Update install.sh

--- src/server-install/linux/install.sh
+++ src/server-install/linux/install.sh
@@ -4,14 +4,14 @@
 # Copyright (c) 2020 | Octo Development
 
 clear
-echo ======================
-echo CRYPTT LINUX INSTALLER
-echo ======================
+echo "======================"
+echo "CRYPTT LINUX INSTALLER"
+echo "======================"
 echo
-echo Copyright (c) 2020 Octo Development
+echo "Copyright (c) 2020 Octo Development"
 echo
-echo > Installing Python...
+echo "> Installing Python..."
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -qq python htop < /dev/null > /dev/null
-echo < Complete
+echo "< Complete"
 echo
-echo > Installing Files...
+echo "> Installing Files..."
