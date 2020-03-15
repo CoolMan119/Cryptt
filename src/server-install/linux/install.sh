6385c9d5284dcf747970a0a8e4873709d1f3e280
houseofkraft <CoolMan119@users.noreply.github.com>
2020-03-15 14:32:33 -0400

    Create install.sh

---
+++ src/server-install/linux/install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Linux Installer for Cryppt
+# Copyright (c) 2020 | Octo Development
+
+clear
+echo ======================
+echo CRYPTT LINUX INSTALLER
+echo ======================
+echo
+echo Copyright (c) 2020 Octo Development
+echo
+echo > Installing Python...
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -qq python htop < /dev/null > /dev/null
+echo < Complete
+echo
+echo > Installing Files...
